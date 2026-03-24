f63c9da0b42de2ba08cdf124b3d83baf85ea57ee
Johan Bengtsson-Palme <johan@microbiology.se>
2026-03-24 17:44:06 +0100

    Update annotate_args_de_novo.pl
    Bug fix

--- build-scripts/annotate_args_de_novo.pl
+++ build-scripts/annotate_args_de_novo.pl
@@ -152,7 +152,7 @@ foreach $family (@families) {
                 push(@argNames, $argName);
                 push(@accessions, $full_accession);
             }
-            if ($source eq "Wang_2025")) {
+            if ($source eq "Wang_2025") {
                 #DfrA52_XPO54507.1
                 ($geneName, $accession)= split('_', $rest);
                 $class = lc(substr($geneName, 0 ,3));
